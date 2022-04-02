9ae87173091cd1ab3fc18c518d0469f40c3701fc
taka-yoko <*****@users.noreply.github.com>
2022-04-02 18:27:29 +0900

    number,string,array型

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1, 2, 3];
+let string2: Array<string> = ["tokyo", "osaka"];
+
+let strings: string[] = ["hoge", "fuga"]
+
+let twodimensionarray: number[][] = [
+  [10, 20],
+  [30, 40]
+];
+
+let hairetsu: (string | number | boolean)[] = [1, false, "japan"];
